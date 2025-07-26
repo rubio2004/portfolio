ec6fe20870d79e66216f903d858219a78767f58e
Emmanuel Rubio <rubioemmanuel11@gmail.com>
2025-07-26 13:26:40 +0800

    Updated JS

--- swiper.js
+++ swiper.js
@@ -1,13 +1,17 @@
 function getCoverflowSettings() {
     const width = window.innerWidth;
-    if (width < 640) { // mobile
-        return { stretch: 20, depth: 10, modifier: 8 };
-    } else if (width < 768) { // small tablet
-        return { stretch: 20, depth: 12, modifier: 10 };
-    } else if (width < 1024) { // tablet
-        return { stretch: 35, depth: 18, modifier: 12 };
+    if (width <= 425) { // mobile
+        return { stretch: 10, depth: 20, modifier: 20 };
+    } else if (width <= 640) { // mobile
+        return { stretch: 20, depth: 20, modifier: 20 };
+    } else if (width <= 768) { // small tablet
+        return { stretch: 20, depth: 12, modifier: 25 };
+    } else if (width <= 1024) { // tablet
+        return { stretch: 35, depth: 18, modifier: 20 };
+    } else if (width <= 1440) { // tablet
+        return { stretch: 40, depth: 10, modifier: 28 };
     } else { // desktop
-        return { stretch: 50, depth: 25, modifier: 15 };
+        return { stretch: 50, depth: 8, modifier: 25 };
     }
 }
 
